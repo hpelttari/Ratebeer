c006ddd43e3d1477419235e174a83b89e23c9425
Hannu <hannu.pelttari@gmail.com>
2018-10-22 21:30:21 +0300

    self.top muutettu top

--- app/models/concerns/top.rb
+++ app/models/concerns/top.rb
@@ -1,7 +1,7 @@
 module Top
   extend ActiveSupport::Concern
 
-  def self.top(num)
+  def top(num)
     sorted_by_rating_in_desc_order = all.sort_by{ |b| -(b.average_rating || 0) }
     # palauta listalta parhaat n kappaletta
     # miten? ks. http://www.ruby-doc.org/core-2.5.1/Array.html
